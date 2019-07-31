dbf2b2c9aa879c959b6f73642977bebf9da506ec
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-07-31 17:43:08 -0300

    Sistema de adicionar as tarefas na lista de 'Tarefas realizadas' funcionando

--- js/logica.js
+++ js/logica.js
@@ -1,9 +1,11 @@
 var elementInput = document.querySelector("#menu #add #input")
 var elementButton = document.querySelector("#menu #add #button")
 var containerPendent = document.querySelector("#principal .titulo_tarefas .principal_tarefas")
+var containerResolved = document.querySelector("#principal .titulo_tarefas .principal_resolvidas")
 var tasksList = []
 var resolvedList = []
 
+viewTasks()
 elementButton.onclick = createTask
 
 function createTask(){
@@ -18,7 +20,9 @@ function createTask(){
 
 function viewTasks(){
     containerPendent.innerHTML = ''
+    containerResolved.innerHTML = ''
 
+    //Mostra as tarefas pendentes
     for(i = tasksList.length - 1; i >= 0; i--){
         //Cria a div principal de uma tarefa
         var elementNew = document.createElement("div")
@@ -33,16 +37,14 @@ function viewTasks(){
         var elementRemove = document.createElement("a")
         elementRemove.setAttribute("class", "excluir")
         elementRemove.setAttribute("href", "#")
-        var posicao = tasksList.indexOf()
-        elementRemove.setAttribute("onclick", "removeTask(" + posicao + ")")
+        elementRemove.setAttribute("onclick", "removeTask(" + tasksList.indexOf() + ")")
         elementRemove.appendChild(document.createTextNode("Excluir"))
-
-        
 
         //Cria um link que irá realocar a tarefa para a lista resolvedList
         var elementResolved = document.createElement("a")
         elementResolved.setAttribute("class", "resolvido")
         elementResolved.setAttribute("href", "#")
+        elementResolved.setAttribute("onclick", "resolvedTask(" + i + ")")
         elementResolved.appendChild(document.createTextNode("Resolvido"))
 
         //Cria a div no qual irá ficar os elementos remove e resolved
@@ -57,15 +59,44 @@ function viewTasks(){
         //Poem a div tarefa dentro da div que agrupa todas tarefas
         containerPendent.appendChild(elementNew)
     }
-}
 
-function removeTask(posicao){
-    tasksList.splice(posicao, 1)
-    viewTasks()
+    //Mostra as tarefas concluidas
+    for(i = resolvedList.length - 1; i >= 0; i--){
+        //Cria a div principal de uma tarefa
+        var elementNew = document.createElement("div")
+        elementNew.setAttribute("class", "tarefas")
+
+        //Cria uma div que irá conter o nome da tarefa 
+        var elementTask = document.createElement("div")
+        elementTask.setAttribute("class", "tarefa")
+        elementTask.appendChild(document.createTextNode(resolvedList[i]))
+
+        elementNew.appendChild(elementTask)
+        containerResolved.appendChild(elementNew)
+    }
 
     if(tasksList.length <= 0){
         var element = document.createElement("div")
-        element.appendChild(document.createTextNode("VAZIO"))
+        element.appendChild(document.createTextNode("NENHUMA"))
         containerPendent.appendChild(element)
     }
+    if(resolvedList.length <= 0){
+        var element = document.createElement("div")
+        element.appendChild(document.createTextNode("NENHUMA"))
+        containerResolved.appendChild(element)
+    }
+
+}
+
+//Função ativa quando clicar em resolvido
+function resolvedTask(posicao){
+    resolvedList.push(tasksList[posicao])
+    tasksList.splice(posicao, 1)
+    viewTasks()
+}
+
+//Função ativa quando clicar em excluir
+function removeTask(posicao){
+    tasksList.splice(posicao, 1)
+    viewTasks()
 }
